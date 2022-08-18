d91de527955027331744ca39679a6e7d49bcf2be
Jesus <jesus.fdz93@gmail.com>
2022-08-18 18:34:23 -0500

    grafica y llamada al api listos

--- js/index.js
+++ js/index.js
@@ -1,47 +1,66 @@
 const ctx = document.getElementById("myChart").getContext("2d");
 
-const myChart = new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    plugins: {
-      legend: {
-        labels: {
-          font: {
-            size: 30,
+const makeChart = (dates, positives) => {
+  const myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: dates,
+      datasets: [
+        {
+          label: "Num. de casos",
+          data: positives,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        legend: {
+          labels: {
+            font: {
+              size: 30,
+            },
           },
         },
       },
-    },
-    scales: {
-      y: {
-        beginAtZero: true,
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
       },
     },
-  },
-});
+  });
+};
+
+const getCovidApi = async () => {
+  const resp = await axios.get(
+    "https://api.covidtracking.com/v1/states/tx/daily.json"
+  );
+  const last30 = resp.data.slice(0, 30);
+
+  const dates = last30.map(({ date }) => date);
+  const positives = last30.map(({ positive }) => positive);
+
+  console.log(dates);
+  console.log(positives);
+
+  makeChart(dates, positives);
+};
+
+getCovidApi();
